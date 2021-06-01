04f75898989b5c76ec82fcad91c124849d9e96d7
Dbuescher8 <buescher26@ymail.com>
2021-06-01 18:38:00 -0500

    made progress

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,17 +1,31 @@
 function makeResponsive() {
 
-    var svgArea = d3.select("body").select("svg");
+  var svgArea = d3.select("body").select("svg");
 
-    if (!svgArea.empty()) {
-      svgArea.remove();
-    }
-  
-  var svgWidth = 1100;
-  var svgHeight = 550;
-  
-  var chartMargin = {
-    top: 40,
-    right: 40,
-    bottom: 80,
-    left:100
-  };
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+
+var svgWidth = 1100;
+var svgHeight = 550;
+
+var chartMargin = {
+  top: 40,
+  right: 40,
+  bottom: 80,
+  left:100
+};
+
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+  d3.csv("../assets/data/data.csv").then(function(stateData) {
+
